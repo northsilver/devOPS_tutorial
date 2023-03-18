5eac6cc92df9b6adb8950e30ae1eedac959c063d
Ivan <sh.ivan.m@yandex.ru>
2023-03-18 19:08:35 +0300

    Final

---
+++ Files/07-terraform-02-syntax.tf
@@ -0,0 +1,49 @@
+provider "yandex" {
+  service_account_key_file = "key.json"
+  cloud_id                 = "b1gs6fqvan6645otctmr"
+  folder_id                = "b1gl8p7gd9kjf5d599cd"
+  zone                     = "ru-central1-a"
+}
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2204-lts"
+}
+
+resource "yandex_vpc_network" "net" {
+  name = "net"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet"
+  network_id     = resource.yandex_vpc_network.net.id
+  v4_cidr_blocks = ["10.117.0.0/32"]
+  zone           = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "default" {
+  name        = "netology-test"
+  platform_id = "standard-v2"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores         = 2
+    memory        = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      type     = "network-hdd"
+      size     = "40"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}

de8406baf6700d9d2c6c4572456ffcf97c51129c
Ivan <sh.ivan.m@yandex.ru>
2023-04-07 14:24:06 +0300

    Final

---
+++ terraform/07-04/vpc_module/docs.tf
@@ -0,0 +1,8 @@
+resource "null_resource" "Creation_of_documentation" {
+  provisioner "local-exec" {
+    command = "terraform-docs markdown ${abspath(path.module)} | tee ${abspath(path.module)}/README.md"
+  }
+  triggers = {
+      always_run = "${timestamp()}"
+    }
+}

31fe5f478347259a83bcf3ac7a22638f7f4cb063
Ivan <sh.ivan.m@yandex.ru>
2024-02-13 21:58:02 +0300

    Final

--- Files/diplom/terraform/s3/variables.tf
+++ Files/diplom/terraform/s3/variables.tf
@@ -1,9 +1,9 @@
 variable "yandex_cloud_id" {
-  default = "b1gs6fqvan6645otctmr"
+  default = "b1gs6****ctmr"
 }
 
 variable "yandex_folder_id" {
-  default = "b1gl8p7gd9kjf5d599cd"
+  default = "b1gl8****5d599cd"
 }
 
 variable "zone" {
@@ -11,5 +11,5 @@ variable "zone" {
 }
 
 variable "yc_token_id" {
-  default = "t1.9euelZrMzp2LjZrKmJSQkY3Km8rIku3rnpWazpPIi46Sk5DJiZWYm5WNipHl8_cGCVJR-e8rDwET_t3z90Y3T1H57ysPARP-zef1656VmpiblZfJmomSz83NzJ2amcmM7_zF656VmpiblZfJmomSz83NzJ2amcmM.97pcHzmJ8QAWGrYfz98ZFw-0yidjSbDwaL9a914epKxZ2qxWUyoLRmC2s1CmPsfNdy1XV4-g9hWupVcdEbEFDw"
+  default = "t1.9euelZrMzp2L******pKxZ2qxWUyoLRmC2s1CmPsfNdy1XV4-g9hWupVcdEbEFDw"
 }
